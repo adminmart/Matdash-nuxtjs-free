beb88b1b11676abebea3a4aa24aa5298f66d41f0
wrappixelTeam <114797723+wrappixelTeam@users.noreply.github.com>
2024-10-24 10:04:42 +0530

    ''

---
+++ package/pages/Sample-Page.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import UiParentCard from "@/components/shared/UiParentCard.vue";
+</script>
+<template>
+  <v-row>
+    <v-col cols="12" md="12">
+      <v-card elevation="10">
+        <v-card-item>
+          <h5 class="text-h5 mb-3">Sample Page</h5>  
+          <p class="text-body-1">This is a sample page</p>
+        </v-card-item>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
